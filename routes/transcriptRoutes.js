9e488af197bc27029415f6f0e3acbcb5165a97e5
bini-it <143047673+bini-it@users.noreply.github.com>
2025-05-27 06:35:44 +0300

    Update transcriptRoutes.js

--- routes/transcriptRoutes.js
+++ routes/transcriptRoutes.js
@@ -3,11 +3,13 @@ import {
   getTranscriptByVideoUrl,
   summarizeTranscript,
   getAllVideoTranscripts,
+  getAllVideoSummaries,
 } from '../controllers/transcriptController.js';
 
 const router = express.Router();
 
 router.get('/', getAllVideoTranscripts);
+router.get('/summary', getAllVideoSummaries);
 router.post('/', getTranscriptByVideoUrl);
 router.post('/summary', summarizeTranscript);
 
